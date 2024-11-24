64cfdd72815ad9ee343925617684443c068f5d60
dionisiusnusaca <dnur3no@gmail.com>
2024-11-24 09:54:58 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+const stepItems = ["Dream", "Believe", "Achieve"];
+
+function App() {
+  // let step = 1;
+  // [nama variabel yg menuimpan nilai, nama fungsi yg memunculkan data]
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handleNext() {
+    if (step < 3) {
+      setStep((step) => step + 1);
+    }
+  }
+
+  function handlePrev() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {stepItems[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              className="close"
+              style={{ backgroundColor: "#526D82", color: "#fff" }}
+              onClick={handlePrev}
+            >
+              Prev
+            </button>
+            <button
+              style={{ backgroundColor: "#526D82", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
